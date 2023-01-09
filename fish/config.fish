6d63a00a2ccb6957d3c24f75f83ecd96f6a1f9e9
Sam Clement <sam@shwurl.com>
2023-01-09 17:04:35 +0000

    feat: add default paths for fish to support brew

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+end
