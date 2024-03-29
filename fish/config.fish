ca768cd42bf3d790e5d4ea89de1891bedce467ec
Sam Clement <sam@swhurl.com>
2024-03-29 20:19:01 +0000

    refactor: set default nodejs version as lts

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fish_add_path -g /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
-    set --universal nvm_default_version v18.13.0
+    set --universal nvm_default_version lts
 end
