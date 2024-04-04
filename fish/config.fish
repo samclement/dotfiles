322c3a465c2f34985a6c211e78b36b8b6451f2db
Sam Clement <sam@swhurl.com>
2024-04-04 09:45:42 +0000

    feat: add go binary to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set --universal nvm_default_version lts
+    fish_add_path -m ~/go/bin
+    fish_add_path /usr/local/go/bin
 end
