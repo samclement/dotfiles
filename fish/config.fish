e6fa05ac04633c2acac9fa304383478bb8d0d790
Sam Clement <sam@swhurl.com>
2026-01-02 16:10:04 +0000

    refactor: fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,9 @@ if status is-interactive
     set --universal nvm_default_version lts
     fish_add_path -m ~/go/bin
     fish_add_path /usr/local/go/bin
+    alias k "kubectl"
+    pyenv init - | source
+end
+if test -n "$DISPLAY"
+    bash -c '$HOME/.local/scripts/keymap.sh'
 end
